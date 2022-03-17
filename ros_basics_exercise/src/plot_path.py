23b3f7dc29149a9bfd361ea8326a1249153f35e0
Hugo MIRANDA QUEIROS <hugomirandaqueiros@gmail.com>
2022-03-17 13:07:46 +0100

    first path ok

--- ros_basics_exercise/src/plot_path.py
+++ ros_basics_exercise/src/plot_path.py
@@ -18,26 +18,29 @@ y_w = df_way['y'] * ky
 
 # plot
 f, axs = plt.subplots(1)
-axs.plot(x, y, 'c--')
+axs.plot(x, y, 'c--', label="real path")
 axs.plot(x[0], y[0], 'go')
 axs.annotate("Start", (x[0], y[0]), fontsize=13)
-
-axs.plot([0.0165, 0.0165, -0.0165, -0.0165, 0.0165], [-0.0325, 0.0325, 0.0325, -0.0325, -0.0325], "k")
 
 for i in range(len(x_w)):
     x_i = x_w[i]
     y_i = y_w[i]
-    if i != len(x_w) - 1:
-        axs.plot(x_i, y_i, 'bo')
+    if i == 0:
+        axs.plot(x_i, y_i, 'ro', label="way points")
         axs.annotate(i, (x_i, y_i), fontsize=14)
     else :
         axs.plot(x_i, y_i, 'ro')
-        axs.annotate("End", (x_i, y_i), fontsize=13)
+        axs.annotate(i, (x_i, y_i), fontsize=14)
+
+axs.plot([0.0165, 0.0165, -0.0165, -0.0165, 0.0165], [-0.0325, 0.0325, 0.0325, -0.0325, -0.0325], "b", label="obstacle")
 
 axs.set_aspect('equal')
 axs.set_xlim([-0.40, 0.40])
 axs.set_ylim([-0.30, 0.30])
-
+axs.set_xlabel("x")
+axs.set_ylabel("y")
+axs.set_title("Real path")
+axs.legend(loc='upper left')
 plt.show()
 
 
